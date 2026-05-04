2e95c065cce1c2af65a415a256706e926a953980
Nicolas Toulouse <nicolas.toulouse@stud.uni-heidelberg.de>
2026-05-04 14:25:33 +0200

    refine and design as callable python scripts

--- flexcraft/pipelines/tcr/adapt/design.py
+++ flexcraft/pipelines/tcr/adapt/design.py
@@ -1,45 +1,78 @@
-from flexcraft.pipelines.tcr import *
+from flexcraft.pipelines.tcr.adapt import *
+from flexcraft.pipelines.tcr.utils import *
 import numpy as np
+from typing import List, Tuple, Optional, Dict, Iterable, Callable
+from datetime import datetime
 
 from pathlib import Path
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    usage="Script for running design trials using the ADAPT class."
+)
+
+parser.add_argument("--peptide", nargs="*", default=[],)
+parser.add_argument("--mhc_allele", nargs="*", default=[],)
+parser.add_argument("--binder", nargs="*", default=[],)
+parser.add_argument("--cdrs", type=str, default=None)
+parser.add_argument("--ab", action="store_true")
 
 
-class DesignQueue:
-    def __init(
-    input_dir:Path,
-    cdr_file:str,
-    tcr_dir:str,
-    pmhc_dir:str|None = None,
-    ):
-        self.out = []
-        zipper = []
-        keys = []
-        # check if everything is here
-        if not (input_dir/tcr_dir).exists():
-            raise FileNotFoundError(f"tcr_dir {tcr_dir} not found in input_dir ({input_dir})!")
-        keys.append("scaffold")
-        zipper.append((input_dir/tcr_dir).glob(".pdb"))
-        if not cdr_file is None:
-            if not (input_dir/cdr_file).exists():
-                raise FileNotFoundError(f"cdr_file {cdr_file} not found in input_dir ({input_dir})!")
-            keys.append("cdr3s")
-            zipper.append((
-                l
-                for l in cdr_file
-                ))
-        if not pmhc_dir is None:
-            if not (input_dir/pmhc_dir).exists():
-                raise FileNotFoundError(f"pmhc_dir {pmhc_dir} not found in input_dir ({input_dir})!")
-        self.input_dir = input_dir
-        self.cdr_file = cdr_file
-        self.tcr_dir = tcr_dir
-        self.pmhc_dir = pmhc_dir
-        self.out = [
+parser.add_argument("--config", default="./config.json",)
 
-            for n in zip(*zipper)
-        ]
-    def __call__():
-        return self.out.pop()
+args = parser.parse_args()
+            
 
-    return 
-    pass
+if __name__ == "__main__":
+
+    config = json.load(open(args.config, "r"))
+    mhcs = list(args.mhc_allele)
+    peptides = list(args.peptide)
+    binders = list(args.binder)
+    cdrs_gen = cdr_parser(args.cdrs)
+    
+    if (len(mhcs)>1) and (len(peptides)>1):
+        out_dir = config.get("out_dir", config.get("op_dir", ".")+f"adapt_design_{datetime.now().strftime('%Y-%d-%b_%H:%M:%S')}/")
+    
+    for mhc, peptide in zip(mhcs, peptides):
+        if (len(mhcs)>1) and (len(peptides)>1):
+            config.update({"out_dir":out_dir+f"{mhc}_{peptide}"})
+
+        adapt = ADAPT(
+            **config
+        )
+    
+        print(f"---Designing mhc {mhc} with peptide {peptide}---")
+        mhc_seq = get_mhc(name=mhc)
+
+        for binder in binders:
+            print(f"Using Binder {binder}...")
+            if len(binder)==4:
+                # assume pdb id
+                binder_path = download_structure(
+                    binder,
+                    file_format="antibody" if args.ab else "biological assembly",
+                    out_dir=config["op_dir"]+"input_data"
+                )
+            # else assume pdb path
+            binder_path = clean_chothia(binder_path)
+            
+            cdrs = cdrs_gen()
+
+            scaffold, scaffold_name = adapt.make_scaffold(
+                receptor=binder_path,
+                presenter=mhc_seq,
+                antigen=peptide,
+                cdrs=cdrs,
+                replace_antigen=True,
+            )
+
+            adapt.design_trial(
+                design=scaffold,
+                scaffold_name=scaffold_name,
+                cdrs=list(cdrs.keys())
+            )
+    print(f"Finished design run!")
+    if (len(mhcs)>1) and (len(peptides)>1):
+        print("Collected results at: ",collect_results(Path(out_dir), pattern=f"**/*{adapt.name}*", save=True))
---
+++ flexcraft/pipelines/tcr/adapt/refine.py
@@ -0,0 +1,47 @@
+from flexcraft.pipelines.tcr.adapt import *
+from flexcraft.pipelines.tcr.utils import *
+import numpy as np
+from typing import List, Tuple, Optional, Dict, Iterable, Callable
+
+from pathlib import Path
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    usage="Script for running design trials using the ADAPT class."
+)
+
+parser.add_argument("--designed_dir", type=Path)
+parser.add_argument("--refine_steps", type=int, default=100)
+parser.add_argument("--cdrs", nargs="*")
+
+
+parser.add_argument("--config", default="./config.json",)
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    config = json.load(open(args.config, "r"))
+    # check if subdirs exist
+    out_dir = args.designed_dir
+    if not (out_dir/"scores.csv").exists():
+        raise FileNotFoundError(f"No scores.csv in designed_dir {out_dir}!")
+    config.update(out_dir=out_dir)
+    adapt = ADAPT(
+    **config
+    )
+    for n in range(args.refine_steps):
+        design, pdb_path, design_name = adapt.get_design(
+            index="random"
+        )
+
+        adapt.refine_trial(
+            scaffold=design,
+            scaffold_name=design_name,
+            cdrs=list(args.cdrs)
+        )
+
+    print(f"Finished design run!\nCollecting results...")
+    print("Collected results at: ",collect_results(Path(adapt.out_dir), pattern=f"**/*{adapt.name}*", save=True))
